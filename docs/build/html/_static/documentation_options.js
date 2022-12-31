c3939161bd89287718b1b1ae444bee401a9a8140
clockhart <clockha2@gmu.edu>
2022-12-30 19:31:30 -0500

    dropping code coverage

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.dev48',
+    VERSION: '0.2.3.post0.dev4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
